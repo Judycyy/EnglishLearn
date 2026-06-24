443471e4133d46a43092e6e1432e4341cfd5c926
Judycyy <73261727+Judycyy@users.noreply.github.com>
2026-06-24 09:38:54 +0800

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-﻿window.ENGLISH_LEARN_CONFIG = {
-  // GitHub Pages 是纯静态托管，写在这里的 key 会暴露给访问者。
-  // 如果只是课堂展示，可以把下面的占位符替换成你的 Merriam-Webster Collegiate Dictionary API key。
-  merriamWebsterKey: "c81f82c5-49cb-4056-ac5f-9b555c7af58c",
-};
